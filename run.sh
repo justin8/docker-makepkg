a49051b35dc09f23305c40104b9aec647a7a7845
Justin Dray <justin@dray.be>
2015-06-08 18:54:17 +1000

    Added ability to specify other flags. Still defaults to -fs --noconfirm

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -x
+set -e
 
 cp /src/* /build
 chown -R nobody. /build
 [[ $1 == update ]] && pacman -Syu || pacman -Sy
 
-su nobody -s /bin/bash -c "makepkg -fs --noconfirm"
+if [[ -n $@ ]]
+then
+	flags=$@
+else
+	flags='-fs --noconfirm'
+fi
+su nobody -s /bin/bash -c "makepkg $flags"
 cp /build/*pkg.tar* /src
